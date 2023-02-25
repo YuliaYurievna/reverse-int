3aa3aa53dba32eaca0ab5931e95de21b67ca6d56
Yuliia Poddiacha <104911480+YuliaYurievna@users.noreply.github.com>
2023-02-25 18:15:16 +0200

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+  const positiveNumber = Math.abs(n);
+  const arr = positiveNumber.toString().split('');
+  const reverseNumbers = parseInt(arr.reverse().join(''));
+  console.log('n', typeof reverseNumbers)
+  return reverseNumbers;
   
 }
